ea1a56213e1fb8dc2d4c4597ab1da3613a4a2c4a
PRANETALLU <92874312+PRANETALLU@users.noreply.github.com>
2025-02-19 22:32:59 -0500

    added category attribute

--- server/src/models/Event.js
+++ server/src/models/Event.js
@@ -6,6 +6,7 @@ const EventSchema = new mongoose.Schema({
   dateTime: { type: Date, required: true }, // Stores both date and time
   venue: { type: String, required: true }, // Required, can be 'Online' or a location
   price: { type: Number, default: 0 }, // Free or paid event
+  category: { type: String, required: true },
   organizer: { type: mongoose.Schema.Types.ObjectId, ref: "User", required: true },
   attendees: [{ type: mongoose.Schema.Types.ObjectId, ref: "User" }],
   liveStreamUrl: { type: String, default: "" }, // WebRTC-based stream link
