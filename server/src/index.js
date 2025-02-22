b149d3f02a8661c3d56202ee976c5d7ca337c1a6
ansh-kalariya <akalariya@ufl.edu>
2025-02-22 18:48:32 -0500

    comment out stripe

--- server/src/index.js
+++ server/src/index.js
@@ -19,14 +19,14 @@ const io = socketIo(server, {
 });
 
 app.use(express.json());
-const express = require('express');
+// const express = require('express');
 // const bodyParser = require('body-parser');
 // const paymentRoutes = require('./routes/paymentRoutes');
 // require('dotenv').config();
-app.use(bodyParser.json());
-app.use('/api/payments', paymentRoutes);
-const PORT = process.env.PORT || 5000;
-app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+// app.use(bodyParser.json());
+// app.use('/api/payments', paymentRoutes);
+// const PORT = process.env.PORT || 5000;
+// app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
 
 app.use(cookieParser());
 app.use(
