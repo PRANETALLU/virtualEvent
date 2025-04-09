3dddeaa9e87766d4d4116c227bc4b6a3d91454d5
PRANETALLU <92874312+PRANETALLU@users.noreply.github.com>
2025-04-09 19:03:47 -0400

    Update Payment.js
    Modifying attributes

--- server/src/models/Payment.js
+++ server/src/models/Payment.js
@@ -1,10 +1,10 @@
 const mongoose = require('mongoose');
 
 const PaymentSchema = new mongoose.Schema({
-  ticket: { type: mongoose.Schema.Types.ObjectId, ref: 'Ticket', required: false },
+  user: { type: mongoose.Schema.Types.ObjectId, ref: 'User', required: true },
+  event: { type: mongoose.Schema.Types.ObjectId, ref: 'Event', required: true },
   amount: { type: Number, required: true },
   stripePaymentId: { type: String, required: true },
-  status: { type: String, enum: ['Success', 'Pending', 'Failed'], default: 'Pending' },
   createdAt: { type: Date, default: Date.now },
 });
 
